ac85a6c4a014b602a1ae29e4f60f2c6518cb5996
SweetSocialSpace <sweetsocialspace@gmail.com>
2026-07-17 21:07:46 -0700

    Clean up FeedPage by removing section comments
    Removed comments for layout sections in FeedPage component.

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,15 +1,13 @@
+'use client'
 import Header from '@/app/components/Header'
-
 import PinnedAutomatedAlert from '@/components/PinnedAutomatedAlert'
 import EmergencyAlerts from '@/components/EmergencyAlerts'
 import LatestAlerts from '@/components/LatestAlerts'
 import WhatsHappeningNearYou from '@/components/WhatsHappeningNearYou'
-
 import MarketplacePreview from '@/components/MarketplacePreview'
 import BusinessDirectory from '@/components/BusinessDirectory'
 import UpcomingEvents from '@/components/UpcomingEvents'
 import VerifiedSources from '@/components/VerifiedSources'
-
 import FeedCenter from '@/components/feed/FeedCenter'
 
 export default function FeedPage() {
@@ -17,26 +15,19 @@ export default function FeedPage() {
     <>
       <Header />
       <div className="max-w-7xl mx-auto px-4 py-6 grid grid-cols-1 xl:grid-cols-[340px_1fr_360px] gap-6 items-start">
-
-        {/* LEFT - Alerts and What's Happening */}
         <div className="space-y-6">
           <PinnedAutomatedAlert />
           <EmergencyAlerts />
           <LatestAlerts />
           <WhatsHappeningNearYou />
         </div>
-
-        {/* CENTER - Your new warehouse does ALL the work */}
         <FeedCenter />
-
-        {/* RIGHT - Business stuff */}
         <div className="space-y-4">
           <MarketplacePreview />
           <BusinessDirectory />
           <UpcomingEvents />
           <VerifiedSources />
         </div>
-
       </div>
     </>
   )
