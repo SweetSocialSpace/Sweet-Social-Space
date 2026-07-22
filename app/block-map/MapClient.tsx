7525278c30b13ea3d4579cd8a2044d99e8d4aa35
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-22 00:40:29 -0700

    Add FixSize component to adjust map size

--- app/block-map/MapClient.tsx
+++ app/block-map/MapClient.tsx
@@ -1,14 +1,25 @@
 'use client'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { useEffect } from 'react'
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+function FixSize(){
+  const map = useMap()
+  useEffect(()=>{
+    setTimeout(()=> map.invalidateSize(), 100)
+  },[map])
+  return null
+}
 
 export default function MapClient(){
   return (
-    <MapContainer 
-      center={[37.3382, -121.8863]} 
-      zoom={13} 
-      style={{height:'100vh', width:'100vw', position:'absolute', top:0, left:0}}
+    <MapContainer
+      center={[37.3382, -121.8863]}
+      zoom={13}
+      style={{height:'100vh', width:'100vw'}}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <FixSize />
     </MapContainer>
   )
 }
