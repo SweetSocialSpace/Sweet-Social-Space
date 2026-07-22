58e69ceb057edbd6d53b72c0f7fb87e22d3d007a
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-21 23:19:10 -0700

    Create BlockMapLayout component for layout structure

---
+++ app/block-map/layout.tsx
@@ -0,0 +1,9 @@
+export default function BlockMapLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
+    </html>
+  )
+}
