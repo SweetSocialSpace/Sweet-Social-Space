ea811d0472078330e3f1a70104e846d68a1a0f1d
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-22 00:37:06 -0700

    Enhance MapContainer styling for full viewport
    Updated MapContainer style to include absolute positioning.

--- app/block-map/MapClient.tsx
+++ app/block-map/MapClient.tsx
@@ -3,7 +3,11 @@ import { MapContainer, TileLayer } from 'react-leaflet'
 
 export default function MapClient(){
   return (
-    <MapContainer center={[37.3382, -121.8863]} zoom={13} style={{height:'100vh', width:'100vw'}}>
+    <MapContainer 
+      center={[37.3382, -121.8863]} 
+      zoom={13} 
+      style={{height:'100vh', width:'100vw', position:'absolute', top:0, left:0}}
+    >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
     </MapContainer>
   )
