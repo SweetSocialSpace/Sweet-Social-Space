3de566aeb851d4c8e3edcb4eb57086977757054e
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-22 01:39:36 -0700

    Update MapClient to use custom marker icons
    Refactor MapClient to use custom icon for markers and improve marker popup layout.

--- app/block-map/MapClient.tsx
+++ app/block-map/MapClient.tsx
@@ -5,42 +5,43 @@ import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { createClient } from '@/utils/supabase/client'
 
-delete (L.Icon.Default.prototype as any)._getIconUrl
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+const icon = new L.Icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconSize: [35, 55],
+  iconAnchor: [17, 55],
+  popupAnchor: [0, -50],
   shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 })
 
-function FixSize(){
-  const map = useMap()
-  useEffect(()=>{ setTimeout(()=> map.invalidateSize(), 150)},[map])
-  return null
-}
+function FixSize(){ const map=useMap(); useEffect(()=>{setTimeout(()=>map.invalidateSize(),200)},[map]); return null }
 
 export default function MapClient(){
-  const [pins, setPins] = useState<any[]>([])
-  const supabase = createClient()
+  const [pins,setPins]=useState<any[]>([])
+  const supabase=createClient()
+  useEffect(()=>{ (async()=>{ const {data}=await supabase.from('block_businesses').select('*'); if(data) setPins(data) })() },[])
 
-  useEffect(()=>{
-    const load = async () => {
-      const { data } = await supabase.from('block_businesses').select('*').limit(200)
-      if(data) setPins(data)
-    }
-    load()
-  },[])
-
-  return (
-    <MapContainer center={[37.3382, -121.8863]} zoom={12} style={{height:'100vh', width:'100vw'}}>
+  return(
+    <MapContainer center={[37.3369,-121.8563]} zoom={15} style={{height:'100vh', width:'100vw'}}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <FixSize />
-      {pins.map((b)=>(
-        <Marker key={b.id} position={[b.lat, b.lng]}>
+      {pins.map(b=>(
+        <Marker
+          key={b.id}
+          position={[b.lat,b.lng]}
+          icon={icon}
+          eventHandlers={{
+            mouseover: (e)=> e.target.openPopup(),
+            click: (e)=> e.target.openPopup()
+          }}
+        >
           <Popup>
-            <strong>{b.business_name}</strong><br/>
-            {b.address}<br/>
-            <em>{b.description}</em><br/><br/>
-            <a href={`https://www.google.com/maps/dir/?api=1&destination=${b.lat},${b.lng}`} target="_blank" style={{background:'black', color:'white', padding:'6px 12px', borderRadius:6, textDecoration:'none'}}>Get Directions</a>
+            <div style={{minWidth:150}}>
+              <strong style={{fontSize:16}}>{b.business_name}</strong><br/>
+              {b.address}<br/>
+              <div style={{marginTop:8}}>
+                <a href={`https://www.google.com/maps/dir/?api=1&destination=${b.lat},${b.lng}`} target="_blank" style={{background:'black',color:'white',padding:8,borderRadius:6,textDecoration:'none', display:'inline-block'}}>Get Directions</a>
+              </div>
+            </div>
           </Popup>
         </Marker>
       ))}
