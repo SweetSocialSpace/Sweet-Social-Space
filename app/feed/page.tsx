41e1abaa0a4e9d1025a318b4bac3bf50975949bf
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-25 15:58:30 -0700

    Import TheDrop component in FeedContent

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -27,6 +27,7 @@ import WeatherBar from '@/components/WeatherBar'
 import CreatePost from '@/components/CreatePost'
 import WelcomePost from '@/app/components/WelcomePost'
 import { useLocation } from '@/lib/location-context'
+import TheDrop from '@/app/components/TheDrop'
 
 function FeedContent() {
   const [filter, setFilter] = useState('all')
