9ba5bdeb499c98e0cd25603fcd8d74981174c597
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-24 18:03:55 -0700

    Remove wallpaper import from ProfilePage
    Remove import of wallpaper image from profile page.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { createClient } from '@/lib/supabase/client'
-import wallpaper from '../../golden_droplet_heart_wallpaper.jpg' // <-- ROOT file, this will build
+
 
 export default function ProfilePage(){
   const supabase = createClient()
