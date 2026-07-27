02981546b530b083be3e60fc2f8907ebada1f191
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-26 20:39:56 -0700

    Refactor BlockMapLayout to use div instead of html

--- app/block-map/layout.tsx
+++ app/block-map/layout.tsx
@@ -1,13 +1,3 @@
-export default function BlockMapLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className="block-map-body">
-        {children}
-      </body>
-    </html>
-  )
+export default function BlockMapLayout({ children }: { children: React.ReactNode }) {
+  return <div className="block-map-body min-h-screen">{children}</div>
 }
