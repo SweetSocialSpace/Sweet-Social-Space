29b361059c0d057403bedc5359a70814fa896d19
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-27 13:49:36 -0700

    Remove NearbyLiveFeed from Safe component whitelist
    Removed NearbyLiveFeed from the explicit whitelist in the MAP object.

--- components/Safe.tsx
+++ components/Safe.tsx
@@ -4,12 +4,10 @@ import { HouseErrorBoundary } from './HouseErrorBoundary'
 import { useMemo } from 'react'
 
 const MAP: Record<string, any> = {
-  // explicit whitelist - Next CAN bundle - GLOBAL house-safe
   WeatherBar: () => import('./WeatherBar').then(m => m.default).catch(() => ({ default: () => null })),
   WhatsHappeningNearYou: () => import('./WhatsHappeningNearYou').then(m => m.default || m.WhatsHappeningNearYou).catch(() => ({ default: () => null })),
   VerifiedSources: () => import('./VerifiedSources').then(m => m.default || m.VerifiedSources).catch(() => ({ default: () => null })),
   UpcomingEvents: () => import('./UpcomingEvents').then(m => m.default || m.UpcomingEvents).catch(() => ({ default: () => null })),
-  NearbyLiveFeed: () => import('./NearbyLiveFeed').then(m => m.default || m.NearbyLiveFeed).catch(() => ({ default: () => null })),
   CommunityAlerts: () => import('./CommunityAlerts').then(m => m.default || m.CommunityAlerts).catch(() => ({ default: () => null })),
 }
 
