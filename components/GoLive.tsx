54859cee6ab4df7c5b45715c579fc383d8261891
Harry Sweet <sweetsocialspace@gmail.com>
2026-07-30 16:34:16 -0700

    Update import statement for supabase client

--- components/GoLive.tsx
+++ components/GoLive.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState, useRef, useEffect } from "react";
-import { supabase } from '@/lib/supabase';
+import { createClient } from '@/lib/supabase/client';
 
 type Props = {
   userId?: string;
