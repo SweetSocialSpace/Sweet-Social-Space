20080f34d1e35faf8ff54dbe8dac86b824f9beb2
Harry Sweet <sweetsocialspace@gmail.com>
2026-09-14 07:40:46 -0700

    Simplify ClientProviders by removing unused providers
    Removed LocationProvider and LocationScopeProvider wrappers.

--- app/components/ClientProviders.tsx
+++ app/components/ClientProviders.tsx
@@ -1,18 +1,12 @@
 'use client'
 import { LanguageProvider } from '@/lib/language-context'
 import GlobalTranslator from '@/components/GlobalTranslator'
-import { LocationProvider } from '@/lib/location-context'
-import { LocationScopeProvider } from '@/hooks/useLocationScope'
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <LanguageProvider>
-      <LocationProvider>
-        <LocationScopeProvider>
-          <GlobalTranslator />
-          {children}
-        </LocationScopeProvider>
-      </LocationProvider>
+      <GlobalTranslator />
+      {children}
     </LanguageProvider>
   )
 }
