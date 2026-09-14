a97ffc9567474bb5eac0bc9aa6a2b4c20ab2c58d
Harry Sweet <sweetsocialspace@gmail.com>
2026-09-14 07:39:00 -0700

    Nest GlobalTranslator within LocationProviders

--- app/components/ClientProviders.tsx
+++ app/components/ClientProviders.tsx
@@ -1,7 +1,18 @@
 'use client'
+import { LanguageProvider } from '@/lib/language-context'
 import GlobalTranslator from '@/components/GlobalTranslator'
+import { LocationProvider } from '@/lib/location-context'
+import { LocationScopeProvider } from '@/hooks/useLocationScope'
 
-<LanguageProvider>
-  <GlobalTranslator />
-  {children}
-</LanguageProvider>
+export default function ClientProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <LanguageProvider>
+      <LocationProvider>
+        <LocationScopeProvider>
+          <GlobalTranslator />
+          {children}
+        </LocationScopeProvider>
+      </LocationProvider>
+    </LanguageProvider>
+  )
+}
