e01fe5f8f5a7c071788132d987060867f38b38c4
Harry Sweet <sweetsocialspace@gmail.com>
2026-09-18 07:35:18 -0700

    Remove GlobalTranslator from ClientProviders

--- app/components/ClientProviders.tsx
+++ app/components/ClientProviders.tsx
@@ -1,11 +1,9 @@
 'use client'
 import { LanguageProvider } from '@/lib/language-context'
-import GlobalTranslator from '@/components/GlobalTranslator'
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <LanguageProvider>
-      <GlobalTranslator />
       {children}
     </LanguageProvider>
   )
